aa98778b753160776df78814384287f7add865a2
钟乘永 <zhongcy93@163.com>
2017-03-14 20:18:03 +0800

    代码提交

--- dubboService/src/main/java/com/github/provider/mongo/UserMessageDao.java
+++ dubboService/src/main/java/com/github/provider/mongo/UserMessageDao.java
@@ -3,6 +3,7 @@ package com.github.provider.mongo;
 import com.github.api.entity.UserMessage;
 
 /**
+ * 消息DAO
  * Created by zhongcy on 2016/6/3.
  */
 public interface UserMessageDao extends BaseOptions<UserMessage>{

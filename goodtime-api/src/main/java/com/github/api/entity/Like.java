a0ee31de4b62d97f6e728161458371e88276d101
钟乘永 <zhongcy93@163.com>
2017-03-24 18:18:24 +0800

    lombok引入

--- goodtime-api/src/main/java/com/github/api/entity/Like.java
+++ goodtime-api/src/main/java/com/github/api/entity/Like.java
@@ -1,12 +1,36 @@
 package com.github.api.entity;
 
+
+import lombok.Data;
+
 import java.io.Serializable;
 
 /**
  * 赞
  * Created by zhongcy on 2017-03-24.
  */
-public class Like implements Serializable{
+
+@Data
+public class Like implements Serializable {
     private static final long serialVersionUID = 4680525788796670118L;
 
+    /**
+     * 用户Id
+     */
+    private String userId;
+
+    /**
+     * 用户名
+     */
+    private String userName;
+
+    /**
+     * 点赞时间
+     */
+    private long likeTime;
+
+    /**
+     * 头像URL
+     */
+    private String profileImage;
 }
